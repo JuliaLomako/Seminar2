4623c9f5a8570997e4fd05e28eaa53d45a05fc74
Julia <jlomako@yandex.ru>
2022-03-21 09:32:44 +0300

    задача номер один

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа
+
+int NumB(int num1)
+{
+    int num2 = (num1 / 10);
+    int num3 = num2 % 10;
+   
+    return num3;
+}
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine("из случайного числа " + number + " вторая цифра " + NumB(number));
