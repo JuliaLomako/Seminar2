70727eed4c1cb6b600b3c3c070859a92905094fc
Julia <jlomako@yandex.ru>
2022-03-21 09:35:45 +0300

    задача номер два

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+void NumOrNum(int J)
+{
+if (J > 99)
+{
+   while (J > 999)
+   {
+        J - J / 10;
+    }
+    J - J % 10;
+    Console.WriteLine(J);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+}
+int number = new Random().Next(10, 999);
+Console.WriteLine("случайное число " + number + " max part is " + NumOrNum(number));
+
